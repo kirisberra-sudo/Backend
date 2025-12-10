21c9ba7d4acc8d24e52b653bd6ada5b74be82c73
kirisberra-sudo <kirisberra@gmail.com>
2025-12-10 18:43:50 +0300

    update
    düzenleme

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,5 +1,6 @@
 var mongoose=require("mongoose");
-var dbURI="mongodb://localhost/mekanbul";
+//var dbURI="mongodb://localhost/mekanbul";
+var dbURI=process.env.MONGO_URI;
 mongoose.connect(dbURI);
 mongoose.connection.on("connected",function(){
     console.log("Mongoose "+dbURI+" adresindeki veritabanına bağlandı.");
