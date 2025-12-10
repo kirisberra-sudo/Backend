b0a04515d6213e7a9e954fa9c06b516e3fbc032f
kirisberra-sudo <kirisberra@gmail.com>
2025-12-10 18:01:43 +0300

    güncelleme
    new week update

--- app.js
+++ app.js
@@ -6,14 +6,16 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 require('./app_api/models/db');
+var apiRoute = require('./app_api/routes/index');
 var app = express();
+
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use ('/api', apiRoute);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
---
+++ app_api/routes/index.js
@@ -0,0 +1,49 @@
+var express= require('express');
+var router=express.Router();
+var venueController=require("../controller/venuecontroller");
+var commentController=require("../controller/commentcontroller");
+router
+.route("/venues")
+.get(venueController.listVenues)
+.post(venueController.addVenue);
+
+router
+.route("/venues/:venueid")
+.get(venueController.getVenue)
+.put(venueController.updateVenue)
+.delete(venueController.deleteVenue);
+
+router
+.route("/venues/:venueid/comments")
+.post(commentController.addComment)
+
+router
+.route("/venues/:venueid/comments/:commentid")
+.get(commentController.getComment)
+.put(commentController.updateComment)
+.delete(commentController.deleteComment);
+
+module.exports=router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
